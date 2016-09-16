61ce805154f162484756f4bb6de998aa6cda17e2
Vincenzo Pii <vinc.pii@gmail.com>
2016-09-16 22:34:08 +0200

    Plot decorations

--- src/data_plotter.py
+++ src/data_plotter.py
@@ -1,19 +1,24 @@
 import matplotlib.pyplot as plt
+import os
 
 
 class DataPlotter(object):
 
     def __init__(self):
         super(DataPlotter, self).__init__()
+        # Maps each section name to its xrange
         self._index = {}
-        # List of dicts, each dicts is a curve to be plotted with the reference x values
+        # Dict of dicts, each dicts is a curve to be plotted with the reference x values
+        # {'section_name': {'filename': {points},
+        #                   'filename': {points}}
+        # }
         self._data = {}
 
-    def adddataset(self, name, x_range, data):
+    def adddataset(self, name, x_range, data, filename):
         self._index[name] = x_range
         if not self._data.get(name):
-            self._data[name] = []
-        self._data[name].append(data)
+            self._data[name] = {}
+        self._data[name][filename] = data
 
     def plot(self):
         '''
@@ -21,11 +26,12 @@ class DataPlotter(object):
         '''
         for name in self._index.keys():
             x_range = self._index[name]
-            for points in self._data[name]:
-                plt.plot(x_range, [points[k] for k in x_range])
-                fig = plt.figure()
-                fig.suptitle('bold figure suptitle', fontsize=14, fontweight='bold')
-        # TODO: write title in plot and save it
-        # Title will be name
-        # Legend with curves (filenames?)
+            fig = plt.figure()
+            ax = fig.add_subplot(111)
+            ax.set_title(name)
+            ax.set_ylabel('Wavelength,CircularDichroism')
+            for filenaname, points in self._data[name].items():
+                curve_name = os.path.split(filenaname)[-1]
+                ax.plot(x_range, [points[k] for k in x_range], label=curve_name)
+        plt.legend()
         plt.show()
--- src/main.py
+++ src/main.py
@@ -4,6 +4,7 @@ from data_analyzer import DataAnalyzer
 from data_plotter import DataPlotter
 from cfg_parser import CfgParser
 
+
 def main():
     parser = argparse.ArgumentParser(description='Plot data from unknown tool')
     parser.add_argument('filename', type=str, help='name of the configuration file')
@@ -17,10 +18,11 @@ def main():
         dp = DataPlotter()
         for analysis in analysis_list:
             fp = FileParser()
-            fp.parsefile(analysis['filename'])
+            filename = analysis['filename']
+            fp.parsefile(filename)
             da = DataAnalyzer(fp.get_points(), analysis['concentration'], analysis['chromophores'], analysis['mw'])
             da.process()
-            dp.adddataset(section, cfg.get_x_range(section), da.get_normalized_molar_ellipticity())
+            dp.adddataset(section, cfg.get_x_range(section), da.get_normalized_molar_ellipticity(), filename)
         dp.plot()
 
 if __name__ == "__main__":
